87bd75fba40f6abc46a1dbc3ee0e703dde9eb19c
Ares Yang <yyh.fanatic@gmail.com>
2016-02-25 17:34:10 -0500

    Little Work on Score.php

--- onlineTest/rcs/score.php
+++ onlineTest/rcs/score.php
@@ -4,15 +4,18 @@
 		<div>Welcome to the score page</div>
 <?php
 $file = "anwser.json";
-$fholder = readfile($file);
-$fholder = 
-echo $fholder;
+$fholder = file($file);
+$strHolder = "";
+foreach($fholder as $key => $value){
+	$strHolder .= $value;
+}
+$strArr = json_decode($strHolder,true);
+var_dump($strArr);
 $count = 0;
 foreach($_POST as $key => $value){
 	echo "<p>".$key." ".$value."</p>";
 	$questionNumber = $key[0];
 	$questionAnwser = $key[1];
-
 }
 ?>
 	</body>
