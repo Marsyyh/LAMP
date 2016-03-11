c38428917dd85b081391673ce6704962ea30dea0
Ares Yang <yyh.fanatic@gmail.com>
2016-03-10 19:19:16 -0500

    Finish all click func & data transfer from server

--- phpQPF/rcs/test.js
+++ phpQPF/rcs/test.js
@@ -23,13 +23,37 @@ myApp.factory('sharingDataService',['$http','$rootScope',function($http,$rootSco
 		return request;
 	};
 	//---------------------
-	sharedService.handleClick = function(id){
+	sharedService.handleClick1 = function(id){
 		this.id = id;
-		this.broadcastItem();
+		this.broadcastItem1();
 	};
 	//---------------------
-	sharedService.broadcastItem = function(){
-		$rootScope.$broadcast('handleBroadcast');
+	sharedService.broadcastItem1 = function(){
+		$rootScope.$broadcast('handleBroadcast1');
+	};
+	sharedService.handleClick2 = function(id){
+		this.id = id;
+		this.broadcastItem2();
+	};
+	//---------------------
+	sharedService.broadcastItem2 = function(){
+		$rootScope.$broadcast('handleBroadcast2');
+	};
+	sharedService.handleClick3 = function(id){
+		this.id = id;
+		this.broadcastItem3();
+	};
+	//---------------------
+	sharedService.broadcastItem3 = function(){
+		$rootScope.$broadcast('handleBroadcast3');
+	};
+	sharedService.handleClick4 = function(id){
+		this.id = id;
+		this.broadcastItem4();
+	};
+	//---------------------
+	sharedService.broadcastItem4 = function(){
+		$rootScope.$broadcast('handleBroadcast4');
 	};
 	return sharedService;
 }]).
@@ -45,29 +69,69 @@ controller('column1', ['$scope','sharingDataService', function($scope,sharingDat
 	});
 	//------------creating the click function
 	$scope.c1Click = function(id){
-		sharingDataService.handleClick(id);
+		sharingDataService.handleClick1(id);
 	};
 }])
 .controller('column2', ['$scope','sharingDataService', function($scope,sharingDataService){
-	$scope.$on('handleBroadcast',function(){
+	$scope.$on('handleBroadcast1',function(){
 		var id = sharingDataService.id;
 		var callInData ={
 			"action":"rest",
 			"id":id
 		};
 		sharingDataService.getDataFromServer(callInData).then(function(response){
-			$scope.items = response;
+			var tmpObj = response;
+			$scope.items = tmpObj;
 		});
 	});
+	$scope.c2Click = function(id){
+		sharingDataService.handleClick2(id);
+	}
 }])
-.controller('column3', ['$scope','$http', function($scope,$http){
-	
+.controller('column3', ['$scope','sharingDataService', function($scope,sharingDataService){
+	$scope.$on('handleBroadcast2',function(){
+		var id = sharingDataService.id;
+		var callInData ={
+			"action":"rest",
+			"id":id
+		};
+		sharingDataService.getDataFromServer(callInData).then(function(response){
+			var tmpObj = response;
+			$scope.items = tmpObj;
+		});
+	});
+	$scope.c3Click = function(id){
+		sharingDataService.handleClick3(id);
+	}
 }])
-.controller('column4', ['$scope','$http', function($scope,$http){
-	
+.controller('column4', ['$scope','sharingDataService', function($scope,sharingDataService){
+	$scope.$on('handleBroadcast3',function(){
+		var id = sharingDataService.id;
+		var callInData ={
+			"action":"rest",
+			"id":id
+		};
+		sharingDataService.getDataFromServer(callInData).then(function(response){
+			var tmpObj = response;
+			$scope.items = tmpObj;
+		});
+	});
+	$scope.c4Click = function(id){
+		sharingDataService.handleClick4(id);
+	}
 }])
-.controller('column5', ['$scope','$http', function($scope,$http){
-	
+.controller('column5', ['$scope','sharingDataService', function($scope,sharingDataService){
+	$scope.$on('handleBroadcast4',function(){
+		var id = sharingDataService.id;
+		var callInData ={
+			"action":"rest",
+			"id":id
+		};
+		sharingDataService.getDataFromServer(callInData).then(function(response){
+			var tmpObj = response;
+			$scope.items = tmpObj;
+		});
+	});
 }])
 
 
