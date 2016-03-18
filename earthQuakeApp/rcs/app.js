49a9bcf9e67a551b0901bcae9e970291efc34341
Ares Yang <yyh.fanatic@gmail.com>
2016-03-18 10:38:48 -0400

    Adding marker feature

--- earthQuakeApp/rcs/app.js
+++ earthQuakeApp/rcs/app.js
@@ -5,7 +5,7 @@ app.controller('myEarthQuakeCtrl',function($scope, myEarthQuakeGeoApiService){
         zoom: 4,
         center: {lat: -34.397, lng: 150.644},
     };
-
+    var markers = [];
  	var input = document.getElementById('input');
     var autoComplete = new google.maps.places.Autocomplete(input);
     $scope.earthQuakeMap = new google.maps.Map(document.getElementById('map'), mapOptions);
@@ -18,14 +18,17 @@ app.controller('myEarthQuakeCtrl',function($scope, myEarthQuakeGeoApiService){
 
   		}else{
     		$scope.earthQuakeMap.setCenter(place.geometry.location);
-    		$scope.earthQuakeMap.setZoom(10);
-
+    		$scope.earthQuakeMap.setZoom(5);
+    		clearMaker();
     		var latLng = {
     			'lat':place.geometry.location.lat(),
     			'lng':place.geometry.location.lng()
     		};
     		myEarthQuakeGeoApiService.callGeoApi(latLng.lat,latLng.lng).then(function(response){
     			$scope.items = response.earthquakes;
+    			for (var i = $scope.items.length - 1; i >= 0; i--) {
+    				createMarker($scope.items[i]);
+    			}
     		});
 
     		$scope.handleEnterPress = function($event){
@@ -35,8 +38,27 @@ app.controller('myEarthQuakeCtrl',function($scope, myEarthQuakeGeoApiService){
     		};
  	    }
 	});
+	var createMarker = function(info){
+		var marker = new google.maps.Marker({
+			map:$scope.earthQuakeMap,
+			position: {'lat':info.lat,'lng':info.lng},
+			title: info.magnitude.toString()
+		});
+		markers.push(marker);
+	};
+
+	var setMapOnAll = function(map){
+		for (var i=0; i< markers.length;i++){
+			markers[i].setMap(map);
+		}
+	};
+
+	var clearMaker = function(){
+		setMapOnAll(null);
+		markers = [];
+	};
 })
-.factory('myEarthQuakeGeoApiService',function($http){
+.factory('myEarthQuakeGeoApiService',function($http,$rootScope){
 	var myEarthQuakeGeoApiService = {};
 	myEarthQuakeGeoApiService.callGeoApi = function(lat,lng){
 		var data = {
@@ -58,5 +80,6 @@ app.controller('myEarthQuakeCtrl',function($scope, myEarthQuakeGeoApiService){
 		});
 		return request;
 	};
+	
 	return myEarthQuakeGeoApiService;
 });
